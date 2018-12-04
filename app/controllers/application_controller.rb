20921fd4118918c6d4bd9f42e13246ca3258d483
Martin Bianculli <martin.bianculli@workday.com>
2018-12-04 12:04:40 +0000

    Set P3P headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_p3p_header
+
+  def set_p3p_header
+    headers['P3P'] = 'CP="ALL DSP COR CURa ADMa DEVa OUR IND COM NAV"'
+  end
 end
