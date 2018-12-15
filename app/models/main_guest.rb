b95b27b34daf9c7f01c4d7d1e5914a6c930f7260
Martin Bianculli <martin.bianculli@workday.com>
2018-12-15 16:28:26 +0000

    Fix bug in Main Guest model

--- app/models/main_guest.rb
+++ app/models/main_guest.rb
@@ -3,6 +3,8 @@ class MainGuest < ApplicationRecord
   has_many :guests
 
   def full_name
-    first_name + ' ' + last_name
+    if first_name && last_name
+      first_name + ' ' + last_name
+    end
   end
 end
