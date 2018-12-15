e834256501fdb265ecd4208a6805bafdd27d1f3a
Martin Bianculli <martin.bianculli@workday.com>
2018-12-15 17:58:05 +0000

    Center boxes vertically

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,9 @@ module Rsvp
         'X-Frame-Options' => 'ALLOWALL'
     }
 
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :es
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
