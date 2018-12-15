abeee05ad42ec74104649fdba265ce809cd3c54f
Martin Bianculli <martin.bianculli@workday.com>
2018-12-15 16:53:17 +0000

    Return if no invitations

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -4,7 +4,7 @@ class GuestsController < ApplicationController
 
   def create
     @guest = MainGuest.find_by_email(params[:guest_email])
-    if @guest
+    if @guest && !@guest.invitations.empty?
       redirect_to controller: 'confirmations', action: 'index', guest: Base64.encode64(@guest.email)
     else
       flash[:notice] = "No estas invitado!"
